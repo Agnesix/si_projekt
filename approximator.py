bf864002dfbaba86a977c6ac7e446144a7a1a3c3
kox13 <woloszyndawid99@gmail.com>
2025-05-26 18:42:46 +0200

    detecting and approximating periods

--- approximator.py
+++ approximator.py
@@ -1,6 +1,18 @@
 import numpy as np
+from scipy.signal import find_peaks
+from scipy.interpolate import UnivariateSpline
 
 def polynomial_fit(x, y, degree):
     coeffs = np.polyfit(x, y, degree)
     y_fit = np.polyval(coeffs, x)
     return y_fit, coeffs
+
+def find_periods(y, distance=10, height=0):
+    mean_y = y - np.mean(y)
+
+    corr = np.correlate(mean_y, mean_y, mode='full')
+    corr = corr[len(corr)//2:]
+
+    peaks, _ = find_peaks(corr[1:], distance=distance, height=height)
+
+    return peaks
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from loader import load_breathing_data
-from approximator import polynomial_fit
-from plotter import plot_raw_signal, plot_comparison
+from loader import *
+from approximator import *
+from plotter import *
 
 # Paths to the data
 slow_path = 'data/acc_slow.csv'
@@ -8,24 +8,47 @@ normal_path = 'data/acc_normal.csv'
 hyper_path = 'data/acc_hyper.csv'
 
 # Extract time and amplitude from datasets
-time_slow, amplitude_slow = load_breathing_data(slow_path)
-time_normal, amplitude_normal = load_breathing_data(normal_path)
-time_hyper, amplitude_hyper = load_breathing_data(hyper_path)
+x, y = load_breathing_data(slow_path)
+
+x = x[:len(x)//2]
+y = y[:len(y)//2]
+
+peaks = find_periods(y)
+y_fit = []
+last_peak = 0
+
+for peak in peaks:
+    yf, _ = polynomial_fit(x[last_peak:peak+1], y[last_peak:peak+1], 5)
+    y_fit.extend(yf)
+    last_peak = peak+1
+    plt.axvline(x[peak], color='black', linestyle='--')
+    
+yf, _ = polynomial_fit(x[last_peak:], y[last_peak:], 5)
+y_fit.extend(yf)
+
+# Oryginalny sygnał
+plt.plot(x, y)
+plt.plot(x, y_fit, color='red')
+plt.title("Sygnał z nałożonymi okresami")
+plt.xlabel("Czas [s]")
+plt.ylabel("Amplituda")
+plt.grid(True)
+plt.show()
 
 # Degree of polynomial
-degree = 5
+# degree = 100
 
 # --- Slow breathing ---
-amplitude_slow_fit, _ = polynomial_fit(time_slow, amplitude_slow, degree)
-plot_raw_signal(time_slow, amplitude_slow, 'Slow Breathing – Raw Data', 'Raw slow breathing', color='blue')
-plot_comparison(time_slow, amplitude_slow, amplitude_slow_fit, f'Slow Breathing – Polynomial Approximation (deg {degree})')
+# amplitude_slow_fit, _ = polynomial_fit(time_slow, amplitude_slow, degree)
+# plot_raw_signal(time_slow, amplitude_slow, 'Slow Breathing – Raw Data', 'Raw slow breathing', color='blue')
+# plot_comparison(time_slow, amplitude_slow, amplitude_slow_fit, f'Slow Breathing – Polynomial Approximation (deg {degree})')
 
 # --- Normal breathing ---
-amplitude_normal_fit, _ = polynomial_fit(time_normal, amplitude_normal, degree)
-plot_raw_signal(time_normal, amplitude_normal, 'Normal Breathing – Raw Data', 'Raw normal breathing', color='green')
-plot_comparison(time_normal, amplitude_normal, amplitude_normal_fit, f'Normal Breathing – Polynomial Approximation (deg {degree})')
+# amplitude_normal_fit, _ = polynomial_fit(time_normal, amplitude_normal, degree)
+# plot_raw_signal(time_normal, amplitude_normal, 'Normal Breathing – Raw Data', 'Raw normal breathing', color='green')
+# plot_comparison(time_normal, amplitude_normal, amplitude_normal_fit, f'Normal Breathing – Polynomial Approximation (deg {degree})')
 
 # --- Hyperventilation breathing ---
-amplitude_hyper_fit, _ = polynomial_fit(time_hyper, amplitude_hyper, degree)
-plot_raw_signal(time_hyper, amplitude_hyper, 'Hyperventilation – Raw Data', 'Raw hyperventilation', color='purple')
-plot_comparison(time_hyper, amplitude_hyper, amplitude_hyper_fit, f'Hyperventilation – Polynomial Approximation (deg {degree})')
+# amplitude_hyper_fit, _ = polynomial_fit(time_hyper, amplitude_hyper, degree)
+# plot_raw_signal(time_hyper, amplitude_hyper, 'Hyperventilation – Raw Data', 'Raw hyperventilation', color='purple')
+# plot_comparison(time_hyper, amplitude_hyper, amplitude_hyper_fit, f'Hyperventilation – Polynomial Approximation (deg {degree})')
